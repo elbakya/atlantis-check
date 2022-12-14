3688116b43a83ecf048d140476f6465141c4845d
elbakya <harut.elbakyan@yandex.ru>
2022-12-15 01:22:02 +0400

    adding the workflow

--- local-setup.sh
+++ local-setup.sh
@@ -7,12 +7,12 @@ ATLANTIS_VERSION=v0.21.0
 ATLANTIS_PACKAGE=atlantis_darwin_amd64.zip
 
 echo "Downloading atlantis lib"
-curl -O https://github.com/runatlantis/atlantis/releases/download/v0.21.0/atlantis_linux_386.zip 
+wget -c https://github.com/runatlantis/atlantis/releases/download/v0.21.0/atlantis_linux_386.zip 
 unzip ${ATLANTIS_PACKAGE}
 
 echo "Downloading ngrok: To have access to the internet"
-curl -O https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip
-unzip ngrok-stable-darwin-amd64.zip -d /usr/local/bin
+wget -c https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip -o - \ | tar -xz
+#unzip ngrok-stable-darwin-amd64.zip -d /usr/local/bin
 
 echo "Generate random secret string"
 echo $RANDOM | md5sum | head -c 20; echo;
