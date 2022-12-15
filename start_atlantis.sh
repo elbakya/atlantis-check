fd24dc3211c15617de83903bf2a779c53362dc47
elbakya <harut.elbakyan@yandex.ru>
2022-12-15 09:23:30 +0400

    Added atlantis.yaml file

--- start_atlantis.sh
+++ start_atlantis.sh
@@ -3,10 +3,14 @@ set -e
 
 . ./atlantis.var
 
-atlantis server \
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
+docker run -p 4141:4141 runatlantis/atlantis server \
 --atlantis-url="$URL" \
 --gh-user="$USERNAME" \
 --gh-token="$TOKEN" \
 --gh-webhook-secret="$SECRET" \
---repo-allowlist="$REPO_ALLOWLIST" \
---repo-config="$REPO_CONFIG"
+--repo-allowlist="$REPO_ALLOWLIST" 
+#--repo-config="$REPO_CONFIG"
